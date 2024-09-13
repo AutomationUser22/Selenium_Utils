14b763a59d293d7652d1c516f89b9fd04c1c4015
Vinayak Mahajan <mahajavi@amazon.com>
2024-09-13 21:16:28 +0100

    Added Utils related to Java Scripts

---
+++ src/main/java/util/JavaScriptUtils.java
@@ -0,0 +1,17 @@
+package util;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class JavaScriptUtils {
+    public void scrollToElement(WebDriver driver, WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    public void executeScript(WebDriver driver, String script) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript(script);
+    }
+}
