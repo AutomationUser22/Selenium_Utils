0297e1650c23e756d8fd57fef9ed2ff35355822b
Vinayak Mahajan <mahajavi@amazon.com>
2024-09-13 23:50:24 +0100

    Added logger to BrowserFactoryUtils

--- src/main/java/util/BrowserFactoryUtils.java
+++ src/main/java/util/BrowserFactoryUtils.java
@@ -6,17 +6,19 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BrowserFactoryUtils {
-    public static WebDriver getDriver(String browser) {
-        switch (browser.toLowerCase()) {
-            case "chrome":
-                return new ChromeDriver();
-            case "firefox":
-                return new FirefoxDriver();
-            case "edge":
-                return new EdgeDriver();
-            default:
-                throw new IllegalArgumentException("Unsupported browser: " + browser);
+    public WebDriver getDriver(String browser) {
+        LoggerUtils logger = new LoggerUtils();
+        try {
+            return switch (browser.toLowerCase()) {
+                case "chrome" -> new ChromeDriver();
+                case "firefox" -> new FirefoxDriver();
+                case "edge" -> new EdgeDriver();
+                default -> throw new IllegalArgumentException("Unsupported browser: " + browser);
+            };
+        } catch (Exception e) {
+            logger.warn("Browser Factory Util is not able to create driver", e);
         }
+        return null;
     }
 }
 
