c0d1befcbb71e3f5a1aa75e83256216b09c786c6
Vinayak Mahajan <mahajavi@amazon.com>
2024-09-13 23:55:39 +0100

    Added more methods to generate different type of test data

--- src/main/java/util/TestDataUtils.java
+++ src/main/java/util/TestDataUtils.java
@@ -1,11 +1,23 @@
 package util;
 
+import java.util.Random;
+
 public class TestDataUtils {
     public String generateRandomEmail() {
         return "test" + System.currentTimeMillis() + "@example.com";
     }
 
-    public String getTestData(String key) {
-        return "testData_"+ ((Math.random()*100) + (Math.random()*999));
+    public String generateRandomString(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            sb.append(characters.charAt(random.nextInt(characters.length())));
+        }
+        return sb.toString();
+    }
+
+    public String generateRandomPhoneNumber() {
+        return "+353" + (1000000000 + new Random().nextInt(900000000));
     }
 }
