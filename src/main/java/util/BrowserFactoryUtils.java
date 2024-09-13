7bb43212aab1e49259a00edac02755af49f94dd0
Vinayak Mahajan <mahajavi@amazon.com>
2024-09-14 00:13:12 +0100

    Added Options to BrowserFactory Utils

--- src/main/java/util/BrowserFactoryUtils.java
+++ src/main/java/util/BrowserFactoryUtils.java
@@ -2,16 +2,18 @@ package util;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class BrowserFactoryUtils {
     public WebDriver getDriver(String browser) {
         LoggerUtils logger = new LoggerUtils();
         try {
             return switch (browser.toLowerCase()) {
-                case "chrome" -> new ChromeDriver();
-                case "firefox" -> new FirefoxDriver();
+                case "chrome" -> new ChromeDriver(new ChromeOptions().addArguments("--disable-notifications", "--incognito"));
+                case "firefox" -> new FirefoxDriver(new FirefoxOptions().addArguments("--disable-notifications", "--private"));
                 case "edge" -> new EdgeDriver();
                 default -> throw new IllegalArgumentException("Unsupported browser: " + browser);
             };
