c6458d6df5ddcc322a48c14aef59b0f53985b737
Vinayak Mahajan <mahajavi@amazon.com>
2024-09-13 21:19:18 +0100

    Added util file for Files on machine

---
+++ src/main/java/util/FileUtils.java
@@ -0,0 +1,16 @@
+package util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileUtils {
+    public String readFile(String filePath) throws IOException {
+        return Files.readString(Path.of(filePath));
+    }
+
+    public void writeFile(String filePath, String content) throws IOException {
+        Files.writeString(Path.of(filePath), content);
+    }
+}
+
