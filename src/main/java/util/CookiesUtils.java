4e66435d34d1d023c20ed244bfa90efe3846436e
Vinayak Mahajan <mahajavi@amazon.com>
2024-09-14 00:08:23 +0100

    Removed Static from method signatures as the methods are bound with instance and not with class

--- src/main/java/util/CookiesUtils.java
+++ src/main/java/util/CookiesUtils.java
@@ -4,16 +4,16 @@ import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 
 public class CookiesUtils {
-    public static void addCookie(WebDriver driver, String name, String value) {
+    public void addCookie(WebDriver driver, String name, String value) {
         Cookie cookie = new Cookie(name, value);
         driver.manage().addCookie(cookie);
     }
 
-    public static void deleteCookie(WebDriver driver, String name) {
+    public void deleteCookie(WebDriver driver, String name) {
         driver.manage().deleteCookieNamed(name);
     }
 
-    public static Cookie getCookie(WebDriver driver, String name) {
+    public Cookie getCookie(WebDriver driver, String name) {
         return driver.manage().getCookieNamed(name);
     }
 }
